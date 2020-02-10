ee9159b1e5c61deb85bffce8be9f92e71050e0ed
elloot <elliot.duchek@gmail.com>
2020-02-10 18:42:43 +0100

    fixed some typos

--- popup/flix-a-list.js
+++ popup/flix-a-list.js
@@ -5,12 +5,12 @@ function listenForClicks() {
 }
 
 function reportExecuteScriptError(error) {
-    document.querySelector("#popup-content").classList.add("hidden");
-    document.querySelector("#error-content").classList.remove("hidden");
-    console.error(`Failed to execute beastify content script: ${error.message}`);
+    document.querySelector(".popup-content").classList.add("hidden");
+    document.querySelector(".error-content").classList.remove("hidden");
+    console.error(`Failed to execute Flixalist content script: ${error.message}`);
 }
 
 browser.tabs
-    .executeScript({ file: "/content_scripts/content_scripts" })
+    .executeScript({ file: "../content_scripts/content_script.js" })
     .then(listenForClicks)
     .catch(reportExecuteScriptError);
