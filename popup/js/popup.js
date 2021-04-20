900877ad68fca5349a7eb457cd0bc2012e98c4f7
elloot <elliot.duchek@gmail.com>
2021-04-20 21:04:31 +0200

    reformatted document

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,22 +1,24 @@
-document.querySelectorAll(".button").forEach(element => {
-    element.addEventListener("click", e => {
-        sendMessage("command", e.target.dataset.buttonType);
-    });
+document.querySelectorAll('.button').forEach((element) => {
+  element.addEventListener('click', (e) => {
+    sendMessage('command', e.target.dataset.buttonType);
+  });
 });
 
 function sendMessage(type, message) {
-    browser.tabs.query({ active: true, currentWindow: true }).then(tabs => {
-        browser.tabs.sendMessage(tabs[0].id, {
-            [type]: message
-        });
+  browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, {
+      [type]: message
     });
+  });
 }
 
 function reportExecuteScriptError(error) {
-    document.querySelector(".popup-content").classList.add("hidden");
-    document.querySelector(".error-content").classList.remove("hidden");
-    document.querySelector(".error-content").firstChild.classList;
-    document.querySelector(".error-content").innerHTML = error.message;
+  document.querySelector('.popup-content').classList.add('hidden');
+  document.querySelector('.error-content').classList.remove('hidden');
+  document.querySelector('.error-content').firstChild.classList;
+  document.querySelector('.error-content').innerHTML = error.message;
 }
 
-browser.tabs.executeScript({ file: "../../content_scripts/Content_script.js" }).catch(reportExecuteScriptError);
+browser.tabs
+  .executeScript({ file: '../../content_scripts/Content_script.js' })
+  .catch(reportExecuteScriptError);
